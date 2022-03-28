f2e10149f828ae55bc3a51a007f907298448c49f
hgoncal2 <aluno23327@ipt.pt>
2022-03-28 22:08:00 +0100

    Validacao dos campos dos Donos

--- vet/vet/Data/Migrations/20220328191729_AddEmailDono.Designer.cs
+++ vet/vet/Data/Migrations/20220328191729_AddEmailDono.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using vet.Data;
 
@@ -11,9 +12,10 @@ using vet.Data;
 namespace vet.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220328191729_AddEmailDono")]
+    partial class AddEmailDono
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- vet/vet/Models/Donos.cs
+++ vet/vet/Models/Donos.cs
@@ -1,4 +1,6 @@
-﻿namespace vet.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace vet.Models {
 
     /// <summary>
     /// descrição dos dados dos clientes(donos dos animais) da clínica
@@ -6,29 +8,47 @@
     public class Donos {
 
         public Donos() {
-            ListaAnimais=new HashSet<Animais>();
+            ListaAnimais = new HashSet<Animais>();
         }
 
 
         /// <summary>
         /// PK para o dono
         /// </summary>
+        [Key]
         public int Id { get; set; }
         /// <summary>
         /// Nome do dono do animal
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+        [StringLength(50, ErrorMessage = "O {0} não pode ter mais do que {1} caracteres.")]
+
         public string Nome { get; set; }
         /// <summary>
         /// Sexo do dono
         /// Mm - masculino; Ff - feminino
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+        [StringLength(1, ErrorMessage = "O {0} não pode ter mais do que {1} carácter.")]
+        [RegularExpression("[MmFf]", ErrorMessage = "Só pode escrever F/f para feminino ou M/m para masculino")]
         public string Sexo { get; set; }
         /// <summary>
         /// Número de identificação fiscal do cliente
         /// </summary>
+        [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+        [StringLength(9, MinimumLength = 9, ErrorMessage = "O {0} deve ter exatamente {1} caracteres.")]
+        [RegularExpression("[0-9]{9}")]
         public string NIF { get; set; }
 
-        // lista dos animais do dono
+        /// <summary>
+        /// Email do dono do animal
+        /// </summary>
+        [EmailAddress(ErrorMessage = "Deve escrever um {0} válido.")]
+        public string Email { get; set; }
+
+        /// <summary>
+        /// lista dos animais do dono
+        /// </summary>
         public ICollection<Animais> ListaAnimais { get; set; }
 
     }
